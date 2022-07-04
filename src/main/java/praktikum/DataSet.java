2ad8420c3a23fe48cc2d16be0d554df354163c7e
Eroshchuk Anton <eroschuk@gmail.com>
2022-07-05 03:21:32 +0800

    updated pom.xml Added DataSet class Added test for classes Ingredient and IngredientType

---
+++ src/main/java/praktikum/DataSet.java
@@ -0,0 +1,9 @@
+package praktikum;
+
+public class DataSet {
+    public final String[] ingredientNames = new String[] {"cheese", "tomato", "cucumber", "chipotle", "pesto", "ranch"};
+    public final float[] ingredientPrices = new float[] {150, 100, 50, 20, 20, 20};
+    public final IngredientType[] types = new IngredientType[] {IngredientType.FILLING,IngredientType.FILLING,IngredientType.FILLING,IngredientType.SAUCE,IngredientType.SAUCE,IngredientType.SAUCE};
+
+}
+
